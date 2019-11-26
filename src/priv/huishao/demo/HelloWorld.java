22a9d2132c118b68ba9a016216b0ae7668c70326
binnetwork <lslh0989@163.com>
2019-11-26 21:33:16 +0800

    第一次提交hello,world

--- src/priv/huishao/demo/HelloWorld.java
+++ src/priv/huishao/demo/HelloWorld.java
@@ -1,9 +1,9 @@
 package priv.huishao.demo;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void (mainString[] args) {
         System.out.println("hello,world");
         System.out.println("我不信,烦死了,akfjlakjfda;lkfjda;fdkj");
-        System.out.println("我不信,烦死了。。。。。。。。。。。。。。");
+        System.out.println("我不信,烦死了jkhjkh。。。。。。。。。。。。。。");
     }
 }
