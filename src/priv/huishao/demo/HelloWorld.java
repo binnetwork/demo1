7321fe6267a3c359a7475fb1f8ccdbe432c4357a
binnetwork <lslh0989@163.com>
2019-11-26 21:55:09 +0800

    Merge branch 'master' of https://github.com/binnetwork/demo1
    # Conflicts:
    #	.idea/modules.xml
    #	src/priv/huishao/demo/HelloWorld.java

--- src/priv/huishao/demo/HelloWorld.java
+++ src/priv/huishao/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package priv.huishao.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world");
-        System.out.println("我不信,烦死了");
+        System.out.println("我不信,烦死了afasfdasfdafdafdasfdsa");
         System.out.println("我不信,烦死了。。挨打发发呆发生的的。。申太贵");
         System.out.println("我不信,烦死了,akfjlakjfda;lkfjda;fdkj");
         System.out.println("我不信,烦死了。。。。。。。。。。。。。。");
