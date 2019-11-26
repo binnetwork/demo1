df7f44cae527955c76741ff1528fd6ca8b21d175
e <2126790087@qq.com>
2019-11-26 21:27:06 +0800

    我提交了申太贵2

--- src/priv/huishao/demo/HelloWorld.java
+++ src/priv/huishao/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package priv.huishao.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world");
-        System.out.println("我不信,烦死了");
+        System.out.println("我不信,烦死了。。。。申太贵");
         System.out.println("我不信,烦死了。。。。。。。。。。。。。。");
     }
 }
